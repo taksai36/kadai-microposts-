38b6535c219500530e29edc22ff850d49e8b9f47
sai <nakamura92308@gmail.com>
2018-05-17 01:44:03 +0000

    follow,unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,22 @@
 class UsersController < ApplicationController
-   before_action :require_user_logged_in, only: [:index, :show]
+    before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
     @users = User.all.page(params[:page])
   end
 
   def show
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts(@user)
   end
-  
+
   def new
-     @user = User.new
+    @user = User.new
   end
 
   def create
-   @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
@@ -26,7 +27,19 @@ class UsersController < ApplicationController
     end
   end
 
-private
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
